7df79a72799b4c4935548ccf41eb6995e35c33a2
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-03-30 20:59:06 -0300

    commit

--- Práctica 1/Variables y Constantes y Operadores/practica_1_Ejercicio8.java
+++ Práctica 1/Variables y Constantes y Operadores/practica_1_Ejercicio8.java
@@ -9,6 +9,24 @@ System.out.println(“ false or true es: ” + (false || true)); */
 
 public class practica_1_Ejercicio8 {
     public static void main(String[] args) {
-        
+        operador_logico_and();
+        System.out.println('\0');
+        operador_logico_or();
+    }
+    public static void operador_logico_or() {
+        System.out.println("Tabla de verdad de la disyunción (or)");
+        System.out.println('\0');
+        System.out.println("true OR true es: " + (true||true));
+        System.out.println("true OR false es: " + (true||false));
+        System.out.println("false OR true es: " + (false||true));
+        System.out.println("false OR false es: " + (false||false));
+    }
+    public static void operador_logico_and() {
+        System.out.println("Tabla de verdad de la conjunción (and)");
+        System.out.println('\0');
+        System.out.println("true AND true es: " + (true&&true));
+        System.out.println("true AND false es: " + (true&&false));
+        System.out.println("false AND true es: " + (false&&true));
+        System.out.println("false AND false es: " + (false&&false));
     }
 }
