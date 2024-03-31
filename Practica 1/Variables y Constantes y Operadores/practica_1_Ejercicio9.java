0c8c4fea4aa1dcfe4cd67d752cf6baf1e3e44956
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-03-31 02:33:55 -0300

    incompleto, resultados obtenidos

--- Practica 1/Variables y Constantes y Operadores/practica_1_Ejercicio9.java
+++ Practica 1/Variables y Constantes y Operadores/practica_1_Ejercicio9.java
@@ -4,8 +4,44 @@ por 2, 3, y 4. En base a los resultados obtenidos analice cada
 caso si es correcto o no. */
 
 import java.io.*;
+
 public class practica_1_Ejercicio9 {
     public static void main(String[] args) {
+        int numero;
+
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Ingrese un número entero");
+            numero = Integer.valueOf(entrada.readLine());
+            division_x_2(numero);
+            division_x_3(numero);
+            division_x_4(numero);
+
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    public static void division_x_2(float numero) {
+        float resultado;
+        resultado = (numero/2);
+        System.out.println("La división por 2 es: " + resultado);
+    }
+
+    public static void division_x_3(float numero) {
+        float resultado;
+        resultado = (numero/3);
+        System.out.println("La división por 3 es: " + resultado);
+    }
+
+    public static void division_x_4(float numero) {
+        float resultado;
+        resultado = (numero/4);
+        System.out.println("La división por 4 es: " + resultado);
+    }
+
+    public static void resultados_obtenidos(float numero) {
         
     }
 }
