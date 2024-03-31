f4ab0e01eb5acebdbbf49fc720ea5bb33cdd6605
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-03-31 00:59:37 -0300

    acomodos

--- Practica 1/Variables y Constantes y Operadores/practica_1_Ejercicio8.java
+++ Practica 1/Variables y Constantes y Operadores/practica_1_Ejercicio8.java
@@ -13,7 +13,7 @@ public class practica_1_Ejercicio8 {
         System.out.println('\0');
         operador_logico_or();
     }
-    @SuppressWarnings("unused")
+    @SuppressWarnings("unused") //esto hace que no aparezca el Dead code
     public static void operador_logico_or() {
         System.out.println("Tabla de verdad de la disyunción (or)");
         System.out.println('\0');
@@ -22,7 +22,7 @@ public class practica_1_Ejercicio8 {
         System.out.println("false OR true es: " + (false||true));
         System.out.println("false OR false es: " + (false||false));
     }
-    @SuppressWarnings("unused")
+    // @SuppressWarnings("unused")
     public static void operador_logico_and() {
         System.out.println("Tabla de verdad de la conjunción (and)");
         System.out.println('\0');
