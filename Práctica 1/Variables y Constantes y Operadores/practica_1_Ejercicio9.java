067e79edad43a7c551acfe5a384605d307c5a191
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-03-30 21:06:14 -0300

    continuado e incompleto

--- Práctica 1/Variables y Constantes y Operadores/practica_1_Ejercicio9.java
+++ Práctica 1/Variables y Constantes y Operadores/practica_1_Ejercicio9.java
@@ -1,3 +1,11 @@
+/*Escribir un programa que permita el ingreso de un número entero
+por teclado e imprima el cociente de la división de dicho número
+por 2, 3, y 4. En base a los resultados obtenidos analice cada
+caso si es correcto o no. */
+
+import java.io.*;
 public class practica_1_Ejercicio9 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
