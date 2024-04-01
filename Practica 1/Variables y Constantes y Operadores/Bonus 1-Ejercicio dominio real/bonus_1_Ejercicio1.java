62da665a706ab688f040482f4a5f1656fc174c9c
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-01 15:34:49 -0300

    bonus 1

---
+++ Practica 1/Variables y Constantes y Operadores/Bonus 1-Ejercicio dominio real/bonus_1_Ejercicio1.java
@@ -0,0 +1,22 @@
+/*La cadena de supermercados Carrefive debe imprimir el
+encabezado del ticket de compra de los clientes con cierto formato
+para cumplir las exigencias de la Administración Federal de
+Ingresos PRIVADOS. En el mismo, debe figurar la fecha y hora,
+número de ticket, CUIT, nombre del cajero, número de caja, dni
+del cliente y la leyenda “El precio lo pone el cliente”. Un ejemplo
+se muestra a continuación:
+—-----------------------------------------------------------------
+04/04/23 - 18:55 Ticket Nro. 3455674
+
+CUIT: 99-34567833-9
+
+Cajero: Luis Mercado Caja: 8
+DNI: 33.333.333 “El precio lo pone el cliente”
+—-----------------------------------------------------------------
+Escribir un programa con declaración de constantes y variables
+que pida al usuario el ingreso de los datos necesarios para
+imprimir el encabezado del ticket. */
+
+public class bonus_1_Ejercicio1 {
+    
+}
