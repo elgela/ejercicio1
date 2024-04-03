c2afe29612b7f4660b8be4fda4a045218d408d33
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-03 02:00:51 -0300

    constante tipo int sin valor asignado e igual se pide ingreso por teclado

--- Practica 1/Variables y Constantes y Operadores/1a6-Variables y constantes/practica_1_Ejercicio3.java
+++ Practica 1/Variables y Constantes y Operadores/1a6-Variables y constantes/practica_1_Ejercicio3.java
@@ -2,22 +2,26 @@
 ingreso de un valor por teclado. Luego muestre el valor ingresado.
 ¿Qué pasa en este caso? ¿Se puede o surge algún problema?*/
 
-import java.io.BufferedReader;
+import java.io.BufferedReader; //para tomar la entrada por teclado
 import java.io.InputStreamReader;
 
 public class practica_1_Ejercicio3 {
     public static void main(String[] args) {
-        final int valor;
+        final int valor;// se declara la constante sin un valor
 
         try {
             BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Ingrese valor");
-            valor = Integer.valueOf(entrada.readLine());
-            System.out.println("El valor ingresado ha sido: " + valor);
+            System.out.println("Ingrese valor");// e igual se pide un ingreso por teclado
+            valor = Integer.valueOf(entrada.readLine());// se ingresa valor
+            System.out.println("El valor ingresado ha sido: " + valor);// y funciona bien
         } catch (Exception exc) {
             System.out.println(exc);
         }
     }
 }
 
-//Al tener la variable declarada como constante pero sin un valor asiganado igual pide el ingreso del valor al ser solicitado. Si se hace sin el try_catch no funciona
+/*
+ * Al tener la variable declarada como constante pero sin un valor asignado
+ * igual pide el ingreso del valor al ser solicitado. Si se hace sin el
+ * try_catch no funciona
+ */
