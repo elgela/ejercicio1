4812e6bf25b4b9042e3dc961c317ce718fc963f4
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-03 11:10:56 -0300

    Ingresar 5 numeros y se muestra en orden inverso

--- Practica 1/Variables y Constantes y Operadores/1a6-Variables y constantes/practica_1_Ejercicio4.java
+++ Practica 1/Variables y Constantes y Operadores/1a6-Variables y constantes/practica_1_Ejercicio4.java
@@ -5,6 +5,7 @@ Se debe mostrar: 3, 100, 2, 4, 23 */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class practica_1_Ejercicio4 {
     public static void main(String[] args) {
         int[] numero = new int[5];
@@ -13,10 +14,10 @@ public class practica_1_Ejercicio4 {
             BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
             for (int i = 0; i < 5; i++) {
-                System.out.println("Ingrese número " + (i+1) + ": ");
+                System.out.println("Ingrese número " + (i + 1) + ": ");
                 numero[i] = Integer.valueOf(entrada.readLine());
             }
-            System.out.println('\b');
+            System.out.println('\b'); //deja espacio en blanco
             System.out.println("Números en orden inverso:");
             for (int i = 4; i >= 0; i--) {
                 System.out.println(numero[i]);
