043b651666ae4b5196a62bb52cc20b100b7c77e6
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-09 13:37:40 -0300

    comenzado y no terminado

---
+++ Practica 2/Selectivas e Iterativas/1a5-Selectivas/practica_2_ejercicio1.java
@@ -0,0 +1,113 @@
+/*Escribir un programa completo (declaración de constantes y
+variables, carga de datos, procesamiento e impresión de
+resultados) para cada caso o ítem que:
+a. solicite un número e indique si es positivo o negativo.
+b. solicite un número e imprima “grande” si es mayor a 100 y
+“chico “ si es menor.
+c. solicite un número del 1 al 7 e imprima el día de la semana.
+d. solicite una letra e imprima si es vocal o consonante.
+e. solicite 3 números e imprima “creciente” si los tres números
+están en orden creciente, “decreciente” si están en orden
+decreciente o “error” si no cumplen ningún orden */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class practica_2_ejercicio1 {
+    public static void main(String[] args) {
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        int numero;
+        char letra;
+        
+        try {
+            System.out.println("Ingrese número");
+            numero = Integer.valueOf(entrada.readLine());
+            System.out.println('\0');
+            positivo_negativo(numero);
+            grande_chico(numero);
+            dia_semana(numero);
+            System.out.println('\0');
+            System.out.println("Ingrese una letra");
+            letra = entrada.readLine().charAt(0);
+            vocal_consonante(letra);
+            System.out.println('\0');
+            System.out.println("Ingrese número 1:");
+            numero = Integer.valueOf(entrada.readLine());
+            System.out.println("Ingrese número 2:");
+            numero = Integer.valueOf(entrada.readLine());
+            System.out.println("Ingrese número 3:");
+            numero = Integer.valueOf(entrada.readLine());
+            System.out.println('\0');
+
+        }
+        catch(Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    public static void positivo_negativo(int numero) {
+        if(numero > 0) {
+            System.out.println("Número positivo");
+        }
+        else {
+            System.out.println("Número negativo");
+        }
+
+    }
+
+    public static void grande_chico(int numero) {
+        if (numero > 100) {
+            System.out.println("Grande");
+        }
+        else {
+            System.out.println("Chico");
+        }
+    }
+
+    public static void dia_semana(int numero) {
+        if ((numero >= 1) && (numero <= 7)) {
+            switch (numero) {
+                case 1:
+                    System.out.println("Lunes");
+                    break;
+                case 2:
+                    System.out.println("Martes");
+                    break;
+                case 3:
+                System.out.println("Miércoles");
+                    break;
+                case 4:
+                System.out.println("Jueves");
+                    break;
+                case 5:
+                    System.out.println("Viernes");
+                    break;
+                case 6:
+                    System.out.println("Sábado");
+                    break;
+                case 7:
+                    System.out.println("Domingo");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static void vocal_consonante(char letra) {
+        switch (letra) {
+            case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                System.out.println("Vocal");
+                break;
+            default:
+                System.out.println("Consonante");
+                break;
+        }
+    }
+
+    public static void creciente_decreciente(int numero1, int numero2, int numero3) {
+        if ((numero1>numero2)&&(numero1>numero3)) {
+            
+        }
+    }
+}
