ad9fcc08c0157d5ffa4fcd86aae27e7f74aa748b
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-11 00:04:47 -0300

    ejercicio2 empezado

--- Practica 2/Selectivas e Iterativas/1a5-Selectivas/practica_2_ejercicio2.java
+++ Practica 2/Selectivas e Iterativas/1a5-Selectivas/practica_2_ejercicio2.java
@@ -6,8 +6,29 @@ dependiendo si es bisiesto o no con la siguiente sentencia:
 ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0)))
 imprimir la cantidad de días correspondiente. */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class practica_2_ejercicio2 {
     public static void main(String[] args) {
-        
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        int numero;
+        final int mes = 12;
+        int dias;
+
+        try {
+            System.out.println("Ingrese número de mes:");
+            numero = Integer.valueOf(entrada.readLine());
+            switch (mes) {
+                case 1:
+                    System.out.println("Enero");
+                    break;
+            
+                default:
+                    break;
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
     }
 }
