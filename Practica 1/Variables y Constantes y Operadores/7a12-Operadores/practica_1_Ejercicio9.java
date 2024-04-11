bc670c104b005cfb12324b3f0a2c1bf5796e459f
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-10 23:31:51 -0300

    echo sin métodos y con

--- Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio9.java
+++ Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio9.java
@@ -4,41 +4,65 @@ por 2, 3, y 4. En base a los resultados obtenidos analice cada
 caso si es correcto o no. */
 
 import java.io.*;
-
 public class practica_1_Ejercicio9 {
     public static void main(String[] args) {
-        int numero;
+        Double numero;
 
         try {
             BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-            System.out.println("Ingrese un número entero");
-            numero = Integer.valueOf(entrada.readLine());
+            System.out.println("Ingresar número entero");
+            numero = Double.valueOf(entrada.readLine());
 
-            division_x_2(numero);
-            division_x_3(numero);
-            division_x_4(numero);
-
-        } catch (Exception exc) {
-            System.out.println(exc);
+            double resultado1, resultado2, resultado3;
+            resultado1 = (numero/2);
+            resultado2 = (numero/3);
+            resultado3 = (numero/4);
+            System.out.println("La division por 2 es: " + resultado1);
+            System.out.println("La división por 3 es: " + resultado2);
+            System.out.println("La división por 4 es: " + resultado3);
+            
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
-
-    public static void division_x_2(float numero) {
-        float resultado;
-        resultado = (numero/2);
-        System.out.println("La división por 2 es: " + resultado);
-    }
-
-    public static void division_x_3(float numero) {
-        float resultado;
-        resultado = (numero/3);
-        System.out.println("La división por 3 es: " + resultado);
-    }
-
-    public static void division_x_4(float numero) {
-        float resultado;
-        resultado = (numero/4);
-        System.out.println("La división por 4 es: " + resultado);
-    }
 }
+// import java.io.*;
+
+// public class practica_1_Ejercicio9 {
+//     public static void main(String[] args) {
+//         int numero;
+
+//         try {
+//             BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+//             System.out.println("Ingrese un número entero");
+//             numero = Integer.valueOf(entrada.readLine());
+
+//             division_x_2(numero);
+//             division_x_3(numero);
+//             division_x_4(numero);
+
+//         } catch (Exception exc) {
+//             System.out.println(exc);
+//         }
+//     }
+
+//     public static void division_x_2(float numero) {
+//         float resultado;
+//         resultado = (numero/2);
+//         System.out.println("La división por 2 es: " + resultado);
+//     }
+
+//     public static void division_x_3(float numero) {
+//         float resultado;
+//         resultado = (numero/3);
+//         System.out.println("La división por 3 es: " + resultado);
+//     }
+
+//     public static void division_x_4(float numero) {
+//         float resultado;
+//         resultado = (numero/4);
+//         System.out.println("La división por 4 es: " + resultado);
+//     }
+// }
