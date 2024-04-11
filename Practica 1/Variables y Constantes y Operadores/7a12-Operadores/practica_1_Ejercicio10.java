be4fdf87c0fe6ded889f0081e28385cb4b0cc085
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-10 23:42:17 -0300

    con y sin metodos

--- Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio10.java
+++ Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio10.java
@@ -18,34 +18,52 @@ public class practica_1_Ejercicio10 {
             System.out.println("Ingrese número 2:");
             numero2 = Integer.valueOf(entrada.readLine());
             System.out.println('\0');
-            primero_mayor_segundo(numero1, numero2);
-            multiplos_de_2(numero1, numero2);
+            if (numero1 > numero2) {
+                System.out.println(numero1 + " es mayor a " + numero2);
+            } else {
+                System.out.println("Error! el segundo número fué mayor al primero");
+            }
+            if ((numero1 % 2 == 0) && (numero2 % 2 == 0)) {
+                System.out.println(numero1 + " y " + numero2 + " son múltiplos de 2");
+            }
+            if ((numero1 % 2 != 0)) {
+                System.out.println(numero1 + " no es múltiplo de 2");
+            }
+            if (numero2 % 2 != 0) {
+                System.out.println(numero2 + " no es múltiplo de 2");
+            }
+            if ((numero1 % 2 != 0) && (numero2 % 2 != 0)) {
+                System.out.println("Entonces ni " + numero1 + " ni " + numero2 + " no son múltiplo de 2");
+            }
+    
+            // primero_mayor_segundo(numero1, numero2);
+            // multiplos_de_2(numero1, numero2);
 
         } catch (Exception exc) {
             System.out.println(exc);
         }
     }
-
-    public static void primero_mayor_segundo(int numero1, int numero2) {
-        if (numero1 > numero2) {
-            System.out.println(numero1 + " es mayor a " + numero2);
-        } else {
-            System.out.println("Error! el segundo número fué mayor al primero");
-        }
-    }
-
-    public static void multiplos_de_2(int numero1, int numero2) {
-        if ((numero1 % 2 == 0) && (numero2 % 2 == 0)) {
-            System.out.println(numero1 + " y " + numero2 + " son múltiplos de 2");
-        }
-        if ((numero1 % 2 != 0)) {
-            System.out.println(numero1 + " no es múltiplo de 2");
-        }
-        if (numero2 % 2 != 0) {
-            System.out.println(numero2 + " no es múltiplo de 2");
-        }
-        if ((numero1 % 2 != 0) && (numero2 % 2 != 0)) {
-            System.out.println("Entonces ni " + numero1 + " ni " + numero2 + " no son múltiplo de 2");
-        }
-    }
 }
+    // public static void primero_mayor_segundo(int numero1, int numero2) {
+    //     if (numero1 > numero2) {
+    //         System.out.println(numero1 + " es mayor a " + numero2);
+    //     } else {
+    //         System.out.println("Error! el segundo número fué mayor al primero");
+    //     }
+    // }
+
+    // public static void multiplos_de_2(int numero1, int numero2) {
+    //     if ((numero1 % 2 == 0) && (numero2 % 2 == 0)) {
+    //         System.out.println(numero1 + " y " + numero2 + " son múltiplos de 2");
+    //     }
+    //     if ((numero1 % 2 != 0)) {
+    //         System.out.println(numero1 + " no es múltiplo de 2");
+    //     }
+    //     if (numero2 % 2 != 0) {
+    //         System.out.println(numero2 + " no es múltiplo de 2");
+    //     }
+    //     if ((numero1 % 2 != 0) && (numero2 % 2 != 0)) {
+    //         System.out.println("Entonces ni " + numero1 + " ni " + numero2 + " no son múltiplo de 2");
+    //     }
+    // }
+// }
