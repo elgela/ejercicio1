7169c4be70a4da02a1d5f4000aaf68358d603c1e
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-13 11:23:22 -0300

    cantidad de dígitos

--- Practica 2/Selectivas e Iterativas/1a5-Selectivas/practica_2_ejercicio4.java
+++ Practica 2/Selectivas e Iterativas/1a5-Selectivas/practica_2_ejercicio4.java
@@ -8,5 +8,23 @@ import java.io.InputStreamReader;
 public class practica_2_ejercicio4 {
     public static void main(String[] args) {
         int numero;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        
+        try {
+            System.out.println("Ingrese número entre 0 y 999");
+            numero = Integer.valueOf(entrada.readLine());
+            System.out.println('\0');
+            if ((numero >= 0) && (numero <= 9)) {
+                System.out.println("El número tiene 1 dígito");
+            } else if ((numero >= 10) && (numero <= 99)) {
+                System.out.println("El número tiene 2 dígitos");
+            } else if ((numero >= 100) && (numero <= 999)) {
+                System.out.println("El número tiene 3 dígitos y es " + numero);
+            } else {
+                System.out.println("Debe ser número positivo y de 3 dígitos como máximo");
+            }
+        } catch(Exception e) {
+            System.out.println("Error");
+        }
     }
 }
