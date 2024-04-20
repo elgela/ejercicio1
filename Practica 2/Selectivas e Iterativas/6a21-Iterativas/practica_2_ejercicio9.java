1bf020bdc3fc56425316f91b7e217d530db41df4
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-20 11:22:27 -0300

    ejercicio9 iniciado

---
+++ Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio9.java
@@ -0,0 +1,13 @@
+/*Escribir un programa que mientras que el usuario ingrese un
+carácter dígito o carácter letra minúscula, imprima si es carácter
+dígito o carácter letra minúscula, y si es letra minúscula imprimir si
+es vocal o consonante. */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class practica_2_ejercicio9 {
+    public static void main(String[] args) {
+        
+    }
+}
