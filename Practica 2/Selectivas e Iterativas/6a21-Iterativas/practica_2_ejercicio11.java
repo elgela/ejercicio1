c4643336f62605a1b828dce7a96547b98ce6dbab
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-04-23 16:20:14 -0300

    ejercicio11 completo

---
+++ Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio11.java
@@ -0,0 +1,37 @@
+/*Escribir un programa que mientras el usuario ingrese un
+caracter letra minúscula, acumule la cantidad de vocales que
+ingresó. Finalmente muestre por pantalla dicha cantidad. */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class practica_2_ejercicio11 {
+    public static void main(String[] args) {
+        char letra = 'a';
+        int vocales = 0;
+
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        while (letra != '0') {
+            try {
+                System.out.println("Ingrese letra minúscula / 0 para terminar y contar la cantidad");
+                letra = entrada.readLine().charAt(0);
+                switch (letra) {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        vocales++;
+                        break;
+                
+                    // default:
+                    //     break;
+                }
+            } catch (Exception exc) {
+                System.out.println("Error");
+            }
+        }
+        System.out.println("Cantidad de vocales: " + vocales);
+    }
+}
