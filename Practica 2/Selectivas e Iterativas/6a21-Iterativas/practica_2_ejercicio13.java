40b71b8ab745f65c41da6dbf8ee08970bae0fe10
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-04-24 10:48:50 -0300

    formato acomodado

--- Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio13.java
+++ Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio13.java
@@ -3,16 +3,15 @@ caracter letra minúscula, pida ingresar un número entero. Si el
 número ingresado está entre 1 y 5 inclusive deberá imprimir la
 tabla de multiplicar de dicho número. */
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class practica_2_ejercicio13 {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         char letra = 'a';
         int numero = 1, multiplicador = 1;
         final int MAX = 10;
 
-        
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
         while (letra != '0') {
@@ -23,9 +22,9 @@ public class practica_2_ejercicio13 {
                 if ((letra >= 'a') && (letra <= 'z')) {
                     System.out.println("Ingrese número entero");
                     numero = Integer.valueOf(entrada.readLine());
-                    if ((numero >= 1) && ( numero <= 5)) {
-                        for (multiplicador = 1; multiplicador <= MAX ; multiplicador++) {
-                            System.out.println(numero + " * " + multiplicador + " = " + (numero*multiplicador));
+                    if ((numero >= 1) && (numero <= 5)) {
+                        for (multiplicador = 1; multiplicador <= MAX; multiplicador++) {
+                            System.out.println(numero + " * " + multiplicador + " = " + (numero * multiplicador));
                         }
                     } else {
                         System.out.println("Número incorrecto. Error!");
