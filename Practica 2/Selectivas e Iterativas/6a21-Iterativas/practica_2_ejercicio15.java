2182dfb93a958ab062dc7bc46ec53936d94a5fdd
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-25 17:17:37 -0300

    ejercicio15 incompleto

--- Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio15.java
+++ Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio15.java
@@ -5,18 +5,44 @@ cuál fue el menor número. */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class practica_2_ejercicio15 {
     public static void main(String[] args) {
-        int numero = 1, mayor = 0, menor = 0, cantidad = 0, promedio = 0;
-
+        int numero = 0, suma = 0, cantidad = 0;
+        double promedio = 0, numeroMayor = 0, numeroMenor = 0;
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-        while (numero > 0) {
-            try {
-                
-            } catch (Exception e) {
+        if (numero != 0 ) {
+            numero += numeroMenor;
+            numero += numeroMayor;
+            suma += numero;
+        }
 
+        try {
+            System.out.println("Ingrese un número");
+            numero = Integer.valueOf(entrada.readLine());
+
+            while (numero > 0) {
+                
             }
+        } catch (Exception exc) {
+            System.out.println("Error");
+        }
+
+        }
+
+        public static void promedioNumeros() {
+            int numero = 0, suma = 0, cantidad = 0;
+            double promedio, numeroMayor = 0, numeroMenor = 0;
+
+            if (numero > numeroMayor) {
+                numeroMayor = numero;
+                cantidad++;
+            }
+            if (numero < numeroMenor) {
+                numeroMenor = numero;
+                cantidad++;
+            }
+            promedio = 
         }
     }
-}
