e862fd94bda932c13f5e5e408f7d6fc319e1d120
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-04-26 01:35:30 -0300

    ejercicio15 mal echo sacar porcentaje

--- Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio15.java
+++ Practica 2/Selectivas e Iterativas/6a21-Iterativas/practica_2_ejercicio15.java
@@ -8,41 +8,47 @@ import java.io.InputStreamReader;
 
 public class practica_2_ejercicio15 {
     public static void main(String[] args) {
-        int numero = 0, suma = 0, cantidad = 0;
-        double promedio = 0, numeroMayor = 0, numeroMenor = 0;
+        int numero = 0, suma = 0, cantidad = 0, numeroMayor = 0, numeroMenor = 0;
+        double promedio = 0;
         BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-        if (numero != 0 ) {
+        if (numero != 0) {
+            numero = suma = cantidad;
             numero += numeroMenor;
             numero += numeroMayor;
             suma += numero;
+            promedio = (suma / cantidad);
+
         }
-
-        try {
-            System.out.println("Ingrese un número");
-            numero = Integer.valueOf(entrada.readLine());
-
-            while (numero > 0) {
+        while (numero > 0) {
+            try {
+                System.out.println("Ingrese un número");
+                numero = Integer.valueOf(entrada.readLine());
                 
+                if (numero == 0) {
+                    promedioNumeros(promedio);
+                }
+                
+                System.out.println("Ingrese un número");
+                numero = Integer.valueOf(entrada.readLine());
+            } catch (Exception exc) {
+                System.out.println("Error");
             }
-        } catch (Exception exc) {
-            System.out.println("Error");
-        }
-
-        }
-
-        public static void promedioNumeros() {
-            int numero = 0, suma = 0, cantidad = 0;
-            double promedio, numeroMayor = 0, numeroMenor = 0;
-
-            if (numero > numeroMayor) {
-                numeroMayor = numero;
-                cantidad++;
-            }
-            if (numero < numeroMenor) {
-                numeroMenor = numero;
-                cantidad++;
-            }
-            promedio = 
         }
     }
+
+    public static void promedioNumeros(int numero, int suma, int cantidad, int numeroMayor, int numeroMenor) {
+        double promedio = 0;
+        if (numero > numeroMayor) {
+            numeroMayor = numero;
+            suma = numero;
+            cantidad++;
+        }
+        if (numero < numeroMenor) {
+            numeroMenor = numero;
+            numero = suma;
+            cantidad++;
+        }
+        promedio = (suma / cantidad);
+    }
+}
