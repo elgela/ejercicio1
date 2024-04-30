dd22ed64de6cc45940c25b6705b3a2089e01b99e
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-04-30 20:02:51 -0300

    ejercicio5 empezado

--- Practica 3/Diseño descendente/practica_3_ejercicio5.java
+++ Practica 3/Diseño descendente/practica_3_ejercicio5.java
@@ -4,10 +4,22 @@ valor MAX = 4 (constante). Mientras itera deberá imprimir el
 número. Luego invocarlo desde el programa principal y cuando
 termina, imprimir por pantalla “terminó”. */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+
 public class practica_3_ejercicio5 {
     public static void main(String[] args) {
-
+        incremento();
+        System.out.println("Terminó");
     }
+
+    public static int incremento() {
+        final int MAX = 4;
+        int numero;
+        for (numero = 1; numero < MAX; numero++) {
+            System.out.println("Número " + numero);
+        }
+        return numero;
+    }
+
 }
