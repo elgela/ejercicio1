142303e2c494adb8402cf1535dca6578485779a2
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-05-01 20:33:43 -0300

    cociente división

--- Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio9.java
+++ Practica 1/Variables y Constantes y Operadores/7a12-Operadores/practica_1_Ejercicio9.java
@@ -3,7 +3,8 @@ por teclado e imprima el cociente de la división de dicho número
 por 2, 3, y 4. En base a los resultados obtenidos analice cada
 caso si es correcto o no. */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class practica_1_Ejercicio9 {
     public static void main(String[] args) {
         Double numero;
