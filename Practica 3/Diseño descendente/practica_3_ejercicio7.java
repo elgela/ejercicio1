613b4f01d95dafe3dd011416a089e797d5137686
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-05-01 17:14:18 -0300

    imprime la sumatoria de los primeros 200 num enteros

---
+++ Practica 3/Diseño descendente/practica_3_ejercicio7.java
@@ -0,0 +1,38 @@
+/*Escribir un programa que mientras el usuario cargue desde
+teclado un número entero distinto de 0, imprima por pantalla la
+suma que se obtiene de invocar un método que calcula la
+sumatoria de los primeros 200 números naturales (son números
+enteros entre 1 y 200). */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class practica_3_ejercicio7 {
+    public static void main(String[] args) {
+        /*mientras usuario carga numero distinto de 0
+         * hacer metodo que sume los primeros 200 enteros
+         * imprimir por pantalla al invocarlo
+         */
+        int numero = 1;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Ingrese número");
+            numero = Integer.valueOf(entrada.readLine());
+            if (numero != 0) {
+                suma();
+            }
+        } catch (Exception exc) {
+            System.out.println("Error");
+        }
+    }
+
+    public static int suma() {
+        int suma = 0;
+        final int MAX = 200;
+        for (int num = 0; num <= MAX; num++) {
+            suma = suma + num;
+        }
+        System.out.println("La sumatoria de los primeros 200 números naturales es: " + suma);
+        return suma;
+    }
+}
