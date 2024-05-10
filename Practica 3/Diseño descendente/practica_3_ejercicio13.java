f16544993ca4b97f570502748cc8595266401931
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-05-09 22:54:24 -0300

    no anda

---
+++ Practica 3/Diseño descendente/practica_3_ejercicio13.java
@@ -0,0 +1,80 @@
+/*Realizar un programa que, dada una opción entera (1,2,3,4),
+permita realizar operaciones entre tres números reales (r1,r2,r3)
+ingresados desde teclado. Para la opción:
+1. Calcular la raíz cuadrada de (r1-r3)
+2. Calcular el promedio de r1, r2 y r3
+3. Calcular cociente de la raíz cuadrada de (r3 - r2) dividida por
+r1
+4. Calcular el cociente del promedio de los tres valores dividido
+por la raíz cuadrada de r2
+Observación: La raíz cuadrada de un número se calcula con la
+sentencia: Math.sqrt(numero). Hacer uso del diseño descendente
+y de la modularización para la solución. Tener en cuenta posibles
+errores o excepciones como la división por cero. */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+// import java.util.Scanner;
+public class practica_3_ejercicio13 {
+    public static double r1 = 0.0, r2 = 0.0, r3 = 0.0;
+    public static void main(String[] args) {
+        int opcion = 0;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+            try {
+                System.out.println("Ingrese primer número real");
+                r1 = entrada.readLine().charAt(0);
+                System.out.println("Ingrese segundo número real");
+                r2 = entrada.readLine().charAt(0);
+                System.out.println("Ingrese tercer número real");
+                r3 = entrada.readLine().charAt(0);
+                System.out.println("Elija que realizar");
+                System.out.println("Opción 1: Calcular raiz cuadrada de (r1-r3)");
+                System.out.println("Opción 2: Calcular el promedio de r1, r2 y r3");
+                System.out.println("Opción 3: 3. Calcular cociente de la raíz cuadrada de (r3 - r2) dividida por r1" );
+                System.out.println("Opción 4: Calcular el cociente del promedio de los tres valores dividido por la raíz cuadrada de r2");
+                System.out.println("Ingrese opción");
+                opcion = Integer.valueOf(entrada.readLine());
+    
+                switch (opcion) {
+                    case 1:
+                        // raizCuadrada();
+                        break;
+                    case 2:
+                        promedio();
+                        break;
+                    case 3:
+                        // cocienteRaiz();
+                        break;
+                    case 4:
+                        // cocientePromedio();
+                        break;
+                    // default:
+                    //     break;
+                }
+            } catch (Exception exc) {
+                System.out.println("Error");
+            }
+        }
+
+    // public static double raizCuadrada() {
+    //     Scanner input = new Scanner(System.in);
+    //     float r
+
+    //     return resultado;
+    // }
+
+    public static double promedio() {
+        double prom = (r1 + r2 + r3) / 3.0;
+        System.out.println("El promedio es: " + prom);
+        return prom;
+    }
+
+    // public static double cocienteRaiz() {
+
+    // }
+
+    // public static double cocientePromedio() {
+
+    // }
+}
