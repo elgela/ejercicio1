b887db2d34084792131356f3f200ad0f6a706739
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-05-20 00:38:55 -0300

    Carga arreglo de tamaño 15 y muestra cómo quedó cargado

--- Practica 5/Arreglos/practica_5_ejercicio1.java
+++ Practica 5/Arreglos/practica_5_ejercicio1.java
@@ -1,3 +1,4 @@
+package Arreglos;
 /*Cargar un arreglo de tamaño 15, pidiendo el ingreso por teclado
 de valores entre 1 y 12. Luego mostrar cómo quedó cargado. */
 
@@ -33,35 +34,3 @@ public class practica_5_ejercicio1 {
         }
     }
 }
-// public class Clase_5_Ejemplo_1 {
-// final static int MAX = 5;
-
-// public static void main(String[] args) {
-// int[] enteros = new int[MAX];
-// cargarArreglo(enteros);
-// mostrarArreglo(enteros);
-// }
-
-// public static void cargarArreglo(int[] arrenteros){
-// BufferedReader entrada = new BufferedReader(new
-// InputStreamReader(System.in));
-
-// try{
-// for (int pos = 0; pos < MAX; pos++){
-// System.out.println ("Ingrese el entero de la posición " + pos + ": ");
-
-// arrenteros[pos] = Integer.valueOf(entrada.readLine());
-
-// }
-// } catch(Exception exc){
-// System.out.println(exc);
-
-// }
-// }
-
-// public static void mostrarArreglo(int[] arrenteros) {
-// for (int pos = 0; pos < MAX; pos++) {
-// System.out.println("arrenteros[" + pos + "] -> " + arrenteros[pos]);
-// }
-// }
-// }// fin del class
